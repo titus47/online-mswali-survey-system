a09d4aecbf4890f78bd0320259023eb818ab1d88
William Wafula <wafulatitus31@gmail.com>
2021-08-29 21:22:28 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# online-mswali-survey-system
-survey system
+# Mswali Survey System-
+Survey Management system is a app for setup online survey with so many functionality.
+It is a PHP project.
+
+
+Instalation ::
+
+step 1)Copy full folder in your web directory.
+
+2)Import database in your phpmyadmin named sms.sql.
+
+3)Edit dbconnection file.change username,password and database name.Default user is root,password is null and database name is project.
+
+Default admin emailid is wafulatitus31@gmail.com and password is kitale45.
+admin password is md5 encypted.
+
+
+
---
+++ index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SMS :: SignUp </title>
+    <link  rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>
+    <link rel="stylesheet" href="css/main.css">
+    <link  rel="stylesheet" href="css/font.css">
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+      <div class="header">
+        <div class="row">
+          <div class="col-lg-6">
+            <span class="logo">Mswali Survey System</span></div>
+			<?php
+include_once 'dbConnection.php';
+?>
+<div class="bg">
+
+
+<div class="container"><!--container start-->
+<div class="row">
+<div class="col-md-12">
+
+<!--home start-->
+<?php
+
+$result = mysqli_query($con,"SELECT * FROM survey ORDER BY date DESC") or die('Error');
+echo  '<div class="panel"><table class="table table-striped title1">
+<tr><td><b>S.N.</b></td><td><b>Topic</b></td><td><b>Total question</b></td><td><b>Marks</b></td><td><b>Time limit</b></td><td></td></tr>';
+$c=1;
+while($row = mysqli_fetch_array($result)) {
+	$title = $row['title'];
+	$total = $row['total'];
+	$sahi = $row['sahi'];
+    $time = $row['time'];
+	$eid = $row['eid'];
+$q12=mysqli_query($con,"SELECT score FROM history " )or die('Error98');
+$rowcount=mysqli_num_rows($q12);	
+if($rowcount == 0){
+	echo '<tr><td>'.$c++.'</td><td>'.$title.'</td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
+	<td><b><a href="account.php?q=survey&step=2&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:#99cc32"><span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Start</b></span></a></b></td></tr>';
+}
+else
+{
+echo '<tr style="color:#99cc32"><td>'.$c++.'</td><td>'.$title.'&nbsp;<span title="This survey is already solve by you" class="glyphicon glyphicon-ok" aria-hidden="true"></span></td><td>'.$total.'</td><td>'.$sahi*$total.'</td><td>'.$time.'&nbsp;min</td>
+	<td><b><a href="update.php?q=surveyre&step=25&eid='.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:red"><span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>&nbsp;<span class="title1"><b>Restart</b></span></a></b></td></tr>';
+}
+}
+$c=0;
+echo '</table></div>';
+?>
+
+<!--<span id="countdown" class="timer"></span>
+<script>
+var seconds = 40;
+    function secondPassed() {
+    var minutes = Math.round((seconds - 30)/60);
+    var remainingSeconds = seconds % 60;
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds; 
+    }
+    document.getElementById('countdown').innerHTML = minutes + ":" +    remainingSeconds;
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById('countdown').innerHTML = "Buzz Buzz";
+    } else {    
+        seconds--;
+    }
+    }
+var countdownTimer = setInterval('secondPassed()', 1000);
+</script>-->
+
+<!--home closed-->
+
+<!--survey start-->
+<?php
+if(@$_GET['q']== 'survey' && @$_GET['step']== 2) {
+$eid=@$_GET['eid'];
+$sn=@$_GET['n'];
+$total=@$_GET['t'];
+$q=mysqli_query($con,"SELECT * FROM questions WHERE eid='$eid' AND sn='$sn' " );
+echo '<div class="panel" style="margin:5%">';
+while($row=mysqli_fetch_array($q) )
+{
+$qns=$row['qns'];
+$qid=$row['qid'];
+echo '<b>Question &nbsp;'.$sn.'&nbsp;::<br />'.$qns.'</b><br /><br />';
+}
+$q=mysqli_query($con,"SELECT * FROM options WHERE qid='$qid' " );
+echo '<form action="update.php?q=survey&step=2&eid='.$eid.'&n='.$sn.'&t='.$total.'&qid='.$qid.'" method="POST"  class="form-horizontal">
+<br />';
+
+while($row=mysqli_fetch_array($q) )
+{
+$option=$row['option'];
+$optionid=$row['optionid'];
+echo'<input type="radio" name="ans" value="'.$optionid.'">'.$option.'<br /><br />';
+}
+echo'<br /><button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span>&nbsp;Submit</button></form></div>';
+//header("location:dash.php?q=4&step=2&eid=$id&n=$total");
+}
+//result display
+if(@$_GET['q']== 'result' && @$_GET['eid']) 
+{
+$eid=@$_GET['eid'];
+$q=mysqli_query($con,"SELECT * FROM history WHERE eid='$eid'" )or die('Error157');
+echo  '<div class="panel">
+<center><h1 class="title" style="color:#660033">Result</h1><center><br /><table class="table table-striped title1" style="font-size:20px;font-weight:1000;">';
+
+while($row=mysqli_fetch_array($q) )
+{
+$s=$row['score'];
+$w=$row['wrong'];
+$r=$row['sahi'];
+$qa=$row['level'];
+echo '<tr style="color:#66CCFF"><td>Total Questions</td><td>'.$qa.'</td></tr>
+      <tr style="color:#99cc32"><td>right Answer&nbsp;<span class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span></td><td>'.$r.'</td></tr> 
+	  <tr style="color:red"><td>Wrong Answer&nbsp;<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></td><td>'.$w.'</td></tr>
+	  <tr style="color:#66CCFF"><td>Score&nbsp;<span class="glyphicon glyphicon-star" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
+}
+$q=mysqli_query($con,"SELECT * FROM rank" )or die('Error157');
+while($row=mysqli_fetch_array($q) )
+{
+$s=$row['score'];
+echo '<tr style="color:#990000"><td>Overall Score&nbsp;<span class="glyphicon glyphicon-stats" aria-hidden="true"></span></td><td>'.$s.'</td></tr>';
+}
+echo '</table></div>';
+
+}
+?>
+<!--survey end-->
+<?php
+//history start
+if(@$_GET['q']== 2) 
+{
+$q=mysqli_query($con,"SELECT * FROM history ORDER BY date DESC " )or die('Error197');
+echo  '<div class="panel title">
+<table class="table table-striped title1" >
+<tr style="color:red"><td><b>S.N.</b></td><td><b>survey</b></td><td><b>Question Solved</b></td><td><b>Right</b></td><td><b>Wrong<b></td><td><b>Score</b></td>';
+$c=0;
+while($row=mysqli_fetch_array($q) )
+{
+$eid=$row['eid'];
+$s=$row['score'];
+$w=$row['wrong'];
+$r=$row['sahi'];
+$qa=$row['level'];
+$q23=mysqli_query($con,"SELECT title FROM survey WHERE  eid='$eid' " )or die('Error208');
+while($row=mysqli_fetch_array($q23) )
+{
+$title=$row['title'];
+}
+$c++;
+echo '<tr><td>'.$c.'</td><td>'.$title.'</td><td>'.$qa.'</td><td>'.$r.'</td><td>'.$w.'</td><td>'.$s.'</td></tr>';
+}
+echo'</table></div>';
+}
+
+if(@$_GET['q'] > 2) 
+{
+  echo'
+    <div class="alert alert-default" style="text-align:center;">
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <h4 style="font-size:30px;">Well this embarassing...</h4>
+    <h1 style="font-size:100px;">404 Page</h1>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    </div>
+  ';
+}
+?>
+			
+                          <!--Footer start-->
+                          <div class="row footer">
+                            <div class="col-md-6 box">
+                              <a href="#" data-toggle="modal" data-target="#login">Admin Login</a>
+							  </div>
+                            </div>
+                                    <!--Modal for admin login-->
+                                    <div class="modal fade" id="login">
+                                      <div class="modal-dialog">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                                            <h4 class="modal-title"><span style="color:orange;font-family:'typo' ">ADMIN LOGIN</span></h4>
+                                          </div>
+                                          <div class="modal-body title1">
+                                            <div class="row">
+                                              <div class="col-md-3"></div>
+                                              <div class="col-md-6">
+                                                <form role="form" method="post" action="admin.php?q=index.php">
+                                                  <div class="form-group">
+                                                    <input type="text" name="uname" maxlength="30"  placeholder="Email" class="form-control"/>
+                                                  </div>
+                                                  <div class="form-group">
+                                                    <input type="password" name="password" maxlength="15" placeholder="Password" class="form-control"/>
+                                                  </div>
+                                                  <div class="form-group" align="center">
+                                                    <input type="submit" name="login" value="Login" class="btn btn-primary" />
+                                                  </div>
+                                                </form>
+                                                </div><div class="col-md-3"></div></div>
+                                              </div>
+                                              <!--<div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                              </div>-->
+                                              </div><!-- /.modal-content -->
+                                              </div><!-- /.modal-dialog -->
+                                              </div><!-- /.modal -->
+                                              <!--footer end-->
+                                            </body>
+                                          </html>
